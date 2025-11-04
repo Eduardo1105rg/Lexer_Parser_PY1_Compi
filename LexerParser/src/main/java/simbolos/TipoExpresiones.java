83a4d31f3b269200e69c9c89f6ed5cd9af6905d9
Eduardo1105rg <eduardorojasgomez10@gmail.com>
2025-11-04 14:06:48 -0600

    Correciones en exoresione.

--- LexerParser/src/main/java/simbolos/TipoExpresiones.java
+++ LexerParser/src/main/java/simbolos/TipoExpresiones.java
@@ -5,17 +5,21 @@ public class TipoExpresiones {
     public String tipoDato; // int, char.
 
     public Integer valorI; // null en caso de ser [], entero si es [8].
-    public Float valorF;
+    public Double valorF;
     public Character valorC;
     public String valorS;
     public Boolean valorB;
+
+    public TipoExpresiones(String pTipoDato) {
+        this.tipoDato = pTipoDato;
+    }
 
     public TipoExpresiones(String pTipoDato, Integer tamano) {
         this.tipoDato = pTipoDato;
         this.valorI = tamano;
     }
 
-    public TipoExpresiones(String pTipoDato, Float tamano) {
+    public TipoExpresiones(String pTipoDato, Double tamano) {
         this.tipoDato = pTipoDato;
         this.valorF = tamano;
     }
